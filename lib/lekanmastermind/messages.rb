5e7dcdedd872216ce8f1f1c0a79442876e2903e8
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2016-02-25 01:42:27 +0100

    extracted the messages to a class

---
+++ lib/lekanmastermind/messages.rb
@@ -0,0 +1,77 @@
+require_relative 'computer'
+require_relative 'game'
+
+module Lekanmastermind
+  class Messages
+    def welcome_msg
+      puts 'Welcome to MASTERMIND/n Would you like to (p)lay, read the
+       (i)nstructions or (q)uit?'
+      print '>'
+    end
+
+    def instructions
+      puts 'You know waht to do. You just want me to fucking teeyu'
+    end
+
+    def error_input
+      puts "You've entered an invalid letter. Read the instruction and try again "
+    end
+
+    def select_level_message
+      puts 'Select Your preferred level of difficulty. You can select (b)eginner,
+      (i)ntermediate or (a)dvance'
+    end
+
+    def prompt_for_player1_name
+      puts 'Enter your name'
+    end
+
+    def prompt_for_player2_name
+      puts 'Player two,Enter your name'
+    end
+
+    def enter_input_guess(player_name)
+      puts "Enter your guess #{player_name}"
+    end
+
+    def level_welcome_message(comp_handler)
+      puts "You are playing the #{comp_handler.level} level.
+      You can choose #{comp_handler.number_of_color_and_character[0]} colors from
+      the following colors: #{comp_handler.colours_for_level} To enter a sequence,
+       Enter just the first letters of your  color sequence "
+    end
+
+    def congratulatory_message(player, trial, time)
+      puts "Congratulation #{player.name}!! You guessed the correct sequence
+      '#{player.guess}' in #{trial} trial(s) over #{time / 3600}hrs
+      #{time / 60} min #{time % 60} secs."
+      puts 'Do you want to save your record? y/n'
+    end
+
+    def replay_option
+      puts 'Do you want to play again? Press Y or N'
+    end
+
+    def guess_process(guess, exact, partial, chances)
+      puts "'#{guess.upcase}' has #{exact} exacts and #{partial} partials"
+      puts "#{12 - chances} chances left"
+    end
+
+    def top_ten
+      puts '========= TOP TEN ========='
+    end
+
+    def select_play
+      puts 'Select the mode you want. (s)ingle for single mode or (t)wo for two
+       player mode'
+    end
+
+    def out_of_chance_msg
+      puts "GAME OVER!! You're out of Trials!"
+    end
+
+    def goodbye
+      puts 'Thanks for playing Master Mind. See you soon'
+    end
+  end
+end
