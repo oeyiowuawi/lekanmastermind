24bcb2489a2bf5c098dc528e6c708d942e7792db
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2016-03-03 13:30:05 +0100

    added badges

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
-require 'simplecov'
-SimpleCov.start
-
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter::Formatter
+# require "codeclimate-test-reporter"
+# CodeClimate::TestReporter.start
+# $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+#
+# require 'simplecov'
+# SimpleCov.start
+#
+# require 'codeclimate-test-reporter'
+# CodeClimate::TestReporter::Formatter
+require 'coveralls'
+Coveralls.wear!
+require 'rspec'
 
 require 'lekanmastermind'
